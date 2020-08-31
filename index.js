72f5875a865805c4d5ba86b6d234f2f5bc7618e7
Carla Reis <carla.jusreis@gmail.com>
2020-08-31 10:04:59 -0400

    jwt/login

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "123456"
 
 //using cors
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+function auth(req, res, next){
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+
+        jwt.verify(token,JWTSecret,(err, data) => {
+            if(err){
+                res.status(401);
+                res.json({err: "Token inválida!"});
+            } else {
+                req.token = token;
+                req.loggeUser = {id: data.id, email: data.email};
+                 next();//responsavel por passar a requisição para a rota que o usuario quer acessar
+            }
+        });
+    }else{
+        res.status(401);
+        res.json({err:"Token inválida"});
+    }
+}
 
 //our "pseudo" database in json
 
@@ -37,16 +64,31 @@ let myDB = {
             year: 2020,
             price: 30
         }
+    ], 
+    users: [
+        {
+            id: 1,
+            name: "Victor Lima",
+            email: "victordevtb@guiadoprogramador.com",
+            password: "nodejs"
+        },
+
+        {
+            id: 20,
+            name: "Guilherme",
+            email: "guigg@gmail.com",
+            password: "java123"
+        }
     ]
 }
 
 
-app.get("/games",(req, res) => {
+app.get("/games",auth,(req, res) => {
     res.statusCode = 200
     res.json(myDB.games)
 })
 
-app.get("/games/:id",(req,res) =>{
+app.get("/games/:id",auth,(req,res) =>{
 
     //set a "ERROR" statusCode
     if(isNaN(req.params.id)){
@@ -67,7 +109,7 @@ app.get("/games/:id",(req,res) =>{
     }
 })
 
-app.post("/game",(req,res) =>{
+app.post("/game",auth,(req,res) =>{
 
     let {title, price, year} = req.body
 
@@ -82,7 +124,7 @@ app.post("/game",(req,res) =>{
 })
 
 
-app.delete("/game/:id",(req,res) => {
+app.delete("/game/:id",auth,(req,res) => {
     if(isNaN(req.params.id)){
         res.sendStatus(400)
     }else{
@@ -135,6 +177,39 @@ app.put("/game/:id", (req,res) => {
     }
 }
 })
+
+//rota de login
+app.post("/auth",(req,res) => {
+    var {email, password} = req.body;
+
+    if(email != undefined){
+      let user = myDB.users.find(u => u.email == email);
+
+        if(user != undefined){
+            if(user.password == password){
+                jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn: '48h'},(err, token) => {
+                    if(err){
+                        res.status(400);
+                        res.json({err: "Falha interna"});
+                    } else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                })
+            } else{
+                res.status(401);
+                res.json({err:"Credenciais inválidas!"});
+            }
+
+        } else{
+            res.status(404);
+            res.json({err: "O email enviado não existe na base de dados!"})
+        }
+    }else{
+        res.status(400);
+        res.json({err: "O email enviado é inválido"})
+    }
+});
 
 app.listen(8080,() => {
     console.log("API executing...")
